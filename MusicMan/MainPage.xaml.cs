f15e2cbdd600d35b4c34b34a7fb69bc2ce0916bc
SnarkDev <elia.scherrer@gmx.ch>
2024-02-06 21:46:40 +0100

    Added Youtube Explode package

--- MusicMan/MainPage.xaml.cs
+++ MusicMan/MainPage.xaml.cs
@@ -10,8 +10,9 @@
 
         private void SearchBtn_Clicked(object sender, EventArgs e)
         {
-
             //Mit dem holsch de Text vo de SearchBar, egal ob es mitem Button oder mit Enter aktiviert worde isch->    YouTubeLinkSearchBar.Text;
+            //THX idiot I know how to code
+
             
 
         }
